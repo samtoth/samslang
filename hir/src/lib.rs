22c7b97036f6e67e910d080f2ef454c99807325f
samtoth <sam@toth.co.uk>
2021-03-16 22:02:17 +0000

    Creating definiton for types and starting to add tests for type equality

--- hir/src/lib.rs
+++ hir/src/lib.rs
@@ -7,15 +7,87 @@
 )]
 
 use std::collections::HashMap;
+use std::convert::TryFrom;
 
-trait Type {}
+#[derive(Debug, Clone)]
+struct Tuple(Box<(Type, Type)>);
+pub fn tuple(a: Type, b: Type) -> Type {
+    Type::Tuple(Tuple(Box::new((a,b))))
+}
 
-struct Unit();
+#[derive(Debug, Clone)]
+struct Set();
 
-impl Type for Unit {}
+#[derive(Debug, Clone)]
+struct Mapping(Box<(Type, Type)>);
 
+#[derive(Debug, Clone)]
+enum Type {
+    Unit,
+    Void,
+    Tuple(Tuple),
+    Set(Set),
+    Mapping(Mapping),
+}
+
+impl PartialEq<Type> for Type {
+    fn eq(&self, r: &Type) -> bool {
+        /// 
+    }
+}
+impl Eq for Type {}
+
+#[derive(Hash, Eq, PartialEq)]
 struct Ident(String);
 
-struct Expression(Box<dyn Type>);
+enum ExpressionUntyped {
+    Ident(Ident),
+    Dyadic(Box<(ExpressionUntyped, ExpressionUntyped, ExpressionUntyped)>),
+    Monadic(Box<(ExpressionUntyped, ExpressionUntyped)>),
+    Array(Vec<ExpressionUntyped>),
+}
+
+fn check_function_type(
+    t: &(ExpressionUntyped, ExpressionUntyped, ExpressionUntyped),
+    scope: &Scope,
+) -> Option<Type> {
+    match type_check(&t.1, scope) {
+        Some(e) => match e {
+            Type::Mapping(m) => {
+                let ft = *(m.0);
+                if ft.0 == type_check(&t.0, scope)? && ft.1 == type_check(&t.1, scope)? {
+                    Some(ft.1)
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        },
+        _ => None,
+    }
+}
+
+fn type_check(e: &ExpressionUntyped, scope: &Scope) -> Option<Type> {
+    match e {
+        ExpressionUntyped::Ident(i) => Some(scope.0.get(i)?.1.clone()),
+        ExpressionUntyped::Dyadic(d) => check_function_type(d, scope),
+        _ => None,
+    }
+}
+
+struct Expression(ExpressionUntyped, Type);
 
 struct Scope(HashMap<Ident, Expression>);
+
+#[cfg(test)]
+mod tests {
+use super::Type;
+use super::tuple;
+    #[test]
+    fn tuple_assoc() {
+        assert_eq!(
+            tuple(Type::Void, tuple(Type::Void, Type::Void)),
+            tuple(tuple(Type::Void, Type::Void),Type::Void),
+            );
+    }
+}
