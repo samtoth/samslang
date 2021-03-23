cc97901bbba052c09c3c65e28441e527017125c0
samtoth <sam@toth.co.uk>
2021-03-23 08:51:32 +0000

    Implementing basic type algebra

--- hir/src/lib.rs
+++ hir/src/lib.rs
@@ -7,22 +7,55 @@
 )]
 
 use std::collections::HashMap;
-use std::convert::TryFrom;
 
 #[derive(Debug, Clone)]
-struct Tuple(Box<(Type, Type)>);
+pub struct Tuple(Box<(Type, Type)>);
+/// return a tuple of 2 types
 pub fn tuple(a: Type, b: Type) -> Type {
-    Type::Tuple(Tuple(Box::new((a,b))))
+    Type::Tuple(Tuple(Box::new((a, b))))
+}
+
+impl Tuple {
+    pub fn cardinality(&self) -> Cardinal {
+        (*self.0).0.cardinality() * (*self.0).1.cardinality()
+    }
 }
 
 #[derive(Debug, Clone)]
-struct Set();
+pub enum Predicate {
+    TypeEq(Type),
+}
+
+impl Predicate {
+    pub fn cardinality(&self) -> Cardinal {
+        match self {
+            Predicate::TypeEq(t) => t.cardinality(),
+        }
+    }
+
+    pub fn eval(&self) -> bool {
+        match self {
+            _ => false,
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
-struct Mapping(Box<(Type, Type)>);
+pub struct Set(Vec<Predicate>);
+
+impl Set {
+    pub fn cardinality(&self) -> Cardinal {
+        self.0
+            .iter()
+            .fold(Cardinal::Finite(0), |i, p| i + p.cardinality())
+    }
+}
 
 #[derive(Debug, Clone)]
-enum Type {
+pub struct Mapping(Box<(Type, Type)>);
+
+#[derive(Debug, Clone)]
+pub enum Type {
     Unit,
     Void,
     Tuple(Tuple),
@@ -30,9 +63,68 @@ enum Type {
     Mapping(Mapping),
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Cardinal {
+    Infinite,
+    Finite(usize),
+}
+impl std::ops::Mul for Cardinal {
+    fn mul(self, r: Cardinal) -> Cardinal {
+        match (self, r) {
+            (Cardinal::Infinite, _) | (_, Cardinal::Infinite) => Cardinal::Infinite,
+            (Cardinal::Finite(a), Cardinal::Finite(b)) => Cardinal::Finite(a * b),
+        }
+    }
+    type Output = Cardinal;
+}
+
+impl std::ops::Add for Cardinal {
+    fn add(self, r: Cardinal) -> Cardinal {
+        match (self, r) {
+            (Cardinal::Infinite, _) | (_, Cardinal::Infinite) => Cardinal::Infinite,
+            (Cardinal::Finite(a), Cardinal::Finite(b)) => Cardinal::Finite(a + b),
+        }
+    }
+    type Output = Cardinal;
+}
+
+impl Type {
+    pub fn cardinality(&self) -> Cardinal {
+        match self {
+            Type::Void => Cardinal::Finite(0),
+            Type::Unit => Cardinal::Finite(1),
+            Type::Tuple(t) => t.cardinality(),
+            Type::Set(s) => s.cardinality(),
+            Type::Mapping(_) => Cardinal::Finite(0),
+        }
+    }
+}
+
+impl std::ops::Add<Type> for Type {
+    type Output = Type;
+    fn add(self, b: Type) -> <Self as std::ops::Add<Type>>::Output {
+        sum(self, b)
+    }
+}
+
+impl std::ops::Mul<Type> for Type {
+    fn mul(self, b: Type) -> <Self as std::ops::Mul<Type>>::Output {
+        product(self, b)
+    }
+    type Output = Type;
+}
+
+pub fn product(a: Type, b: Type) -> Type {
+    tuple(a, b)
+}
+
+pub fn sum(a: Type, b: Type) -> Type {
+    Type::Set(Set(vec![Predicate::TypeEq(a), Predicate::TypeEq(b)]))
+}
+
 impl PartialEq<Type> for Type {
     fn eq(&self, r: &Type) -> bool {
-        /// 
+        self.cardinality() == r.cardinality()
     }
 }
 impl Eq for Type {}
@@ -81,13 +173,36 @@ struct Scope(HashMap<Ident, Expression>);
 
 #[cfg(test)]
 mod tests {
-use super::Type;
-use super::tuple;
+    use super::tuple;
+    use super::Type;
+    use crate::Cardinal;
     #[test]
     fn tuple_assoc() {
         assert_eq!(
             tuple(Type::Void, tuple(Type::Void, Type::Void)),
-            tuple(tuple(Type::Void, Type::Void),Type::Void),
-            );
+            tuple(tuple(Type::Void, Type::Void), Type::Void),
+        );
+    }
+
+    use super::{product, sum};
+    #[test]
+    fn type_identity() {
+        let plus = Type::Unit + Type::Void;
+        eprintln!("Cardinality of {:?} = {:?}", plus, plus.cardinality());
+        assert_eq!(Type::Unit + Type::Void, Type::Unit);
+        assert_eq!(Type::Unit * Type::Void, Type::Void);
+    }
+
+    #[test]
+    fn cardinal_add() {
+        assert_eq!(
+            Type::Unit.cardinality() + Type::Unit.cardinality(),
+            Cardinal::Finite(2)
+        );
+
+        assert_eq!(
+            Type::Unit.cardinality() + Type::Void.cardinality(),
+            Cardinal::Finite(1)
+        );
     }
 }
